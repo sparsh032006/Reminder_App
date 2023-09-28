da7a9797b32332584a7527b4a3345b2275c14425
sparsh032006 <146342420+sparsh032006@users.noreply.github.com>
2023-09-28 17:34:08 +0530

    Add files via upload

---
+++ Reminder/Reminder-main/js/userData.js
@@ -0,0 +1,12 @@
+[
+    {
+        "username": "sparsh",
+        "password": "password1",
+        "email": "user1@example.com"
+    },
+    {
+        "username": "user2",
+        "password": "password2",
+        "email": "user2@example.com"
+    }
+]
